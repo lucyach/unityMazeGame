502ee0e9b0f2d2e57c838948591cd62322e22963
Lucy Acheson <70671534+lucyach@users.noreply.github.com>
2021-03-17 20:11:27 -0700

    Add files via upload
    more c# code!

---
+++ movingBlock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingBlock : MonoBehaviour
+{
+    public float speed;
+    public float leftWall;
+    public float rightWall;
+    public Vector3 startPos;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        speed = 8;
+        leftWall = transform.position.x;
+        rightWall = leftWall+10;
+        startPos = transform.position;
+        return null;
+    }
+
+    // Update is called once per frame
+    IEnumerator Update()
+    {
+        // transform.Translate(speed*Time.deltaTime*Vector3.right);
+        yield return StartCoroutine("goRight");
+    }
+
+    IEnumerator goRight()
+    {
+        transform.position = Vector3.Lerp(startPos, startPos + 10*(Vector3.right), 2);
+        return null;
+    }
+}
