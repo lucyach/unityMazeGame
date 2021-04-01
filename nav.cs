6e4421f1ba1242c31909768c84a8af62fa899586
Lucy Acheson <70671534+lucyach@users.noreply.github.com>
2021-03-31 19:56:58 -0700

    new work

--- nav.cs
+++ nav.cs
@@ -7,6 +7,7 @@ public class nav : MonoBehaviour
 {
     public GameObject destination;
     NavMeshAgent agent;
+    static int cloneCount = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,23 @@ public class nav : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(destination.transform.position);
+        Vector3 mousePos = Input.mousePosition;
+        Ray correctMousePos = Camera.main.ScreenPointToRay(mousePos);
+        RaycastHit mostCorrectMousePos;
+        if (Physics.Raycast(correctMousePos, out mostCorrectMousePos)) {
+            agent.SetDestination(mostCorrectMousePos.point);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject == destination && cloneCount < 15) {
+            Instantiate(this.gameObject, this.transform.position, Quaternion.identity);
+            cloneCount++;
+            StartCoroutine(delay());
+        }
+    }
+
+    IEnumerator delay() {
+        yield return new WaitForSeconds(5);
     }
 }
