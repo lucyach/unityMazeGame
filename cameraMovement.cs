8eefe72d83029991efcc0101d843a8ab0f93c2cd
Lucy Acheson <70671534+lucyach@users.noreply.github.com>
2021-04-14 20:00:40 -0700

    erika update

---
+++ cameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this.transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+    	this.transform.Rotate(new Vector3((-1 * Input.GetAxis("Mouse Y")), 0, 0));
+    }
+}
---
+++ erikaMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class erikaMovement : MonoBehaviour
+{
+    float x;
+    float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = 0;
+        z = 0;
+        Screen.lockCursor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(new Vector3(Input.GetAxis("Horizontal")/2, 0, Input.GetAxis("Vertical")/2));
+    	this.transform.Rotate(new Vector3(0, 5 * Input.GetAxis("Mouse X"), 0));
+    }
+}
